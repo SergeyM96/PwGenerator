06710a834fd706a3f17476de58de9ffb03a947d0
Sergey Morozov <globalvpsm@gmail.com>
2023-09-25 02:08:47 +0300

    added Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar.component';
+import { NavbarComponent } from './Navbar/navbar.component';
 import { PasswordStrengthMeterModule } from './password-strength-meter/password-strength-meter.module';
 import { PasswordHistoryComponent } from './password-history/password-history.component'; // Import the module
 
